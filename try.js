8266587a12db9b5c8379a5989dae274d78b1df14
Ethan Davis <futuristiccake@gmail.com>
2017-10-01 12:18:10 -0700

    Changed organizational structure.

--- try.js
+++ try.js
@@ -4,6 +4,11 @@ let app = new Vaxic()
 
 app.use(Vaxic.static(__dirname))
 
+app.add((req, res) => {
+	console.log(req.url.pathname + ' bye')
+	res.endCompressed('hey', 404)
+})
+
 app.listen(80, () => {
 	console.log('Listening.')
 })
